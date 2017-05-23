24e18588c81cc34eeeda413a195fb82abcbc3613
Tianai Zhao <zhang201pig@126.com>
2017-05-23 00:18:16 -0700

    Add code

---
+++ js/DonutChart.js
@@ -0,0 +1,215 @@
+var DonutChart = function() {
+	var width = 500,
+		height = 500,
+		padAngle,
+		cornerRadius,
+	margin = {
+		left: 70,
+		bottom: 50,
+		top: 30,
+		right: 10,
+	};
+
+	var tooltipFont = 20,
+		tooltipLeft = 140,
+		tooltipTop = 200,
+		tooltipPadding = 10,
+		tooltipWidth = 200;
+
+	var movement = 0;
+
+	var radius = width / 2;
+
+	var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+	var chart = function(selection){
+
+		selection.each(function(data) {
+			var arc = d3.arc()
+				.outerRadius(radius - 10)
+				.innerRadius(radius - 70)
+				.cornerRadius(cornerRadius)
+				.padAngle(padAngle);
+
+			var pie = d3.pie()
+				.sort(null)
+				.value(function(d) {
+					return d.count;
+				});
+
+			var labelArc = d3.arc()
+				.outerRadius(radius - 40)
+				.innerRadius(radius - 40);
+
+			var ele = d3.select(this);
+			var svg = ele.selectAll('svg').data([data]);
+
+			var svgEnter = svg.enter()
+				.append('svg')
+				.attr('width', width)
+				.attr('height', height)
+				.attr('transform', 'translate(' + movement + ", 0)");
+
+
+			svgEnter.append('g')
+				.attr('transform', 'translate(' + width/2 + "," + height/2 + ")")
+				.attr('class', 'arc');
+
+
+			var path = ele.select('.arc').selectAll('path').data(pie(data));
+		
+			path.exit().remove();
+			// 
+			path.enter().append('path')
+				.attr('d', arc)
+				.style('fill', function(d) {
+					return color(d.data.id);
+				})
+				.on('mouseover', function(d){
+					var total = d3.sum(data.map(function(d){
+						return d.count;
+					}))
+
+					var percent = Math.round(1000 * d.data.count / total) / 10;
+					tooltip.select('.label').html(d.data.id);
+					tooltip.select('.count').html(d.data.count);
+					tooltip.select('.percent').html(percent + '%');
+					tooltip.style('display', 'block');
+				})
+				.on('mouseout', function(d){
+					tooltip.style('display', 'none');
+				})
+				.transition()
+				.ease(d3.easeLinear)
+				.duration(1000)
+				.attrTween('d', pieTween);
+				
+			// path.exit().remove();
+
+			var texts = ele.select('.arc').selectAll('text').data(pie(data));
+				texts.enter()
+				.append('text')
+				.merge(texts)
+				.transition()
+				.ease(d3.easeLinear)
+				.duration(1200)
+				.attr('transform', function(d) {
+					return 'translate(' + labelArc.centroid(d) + ')';
+				})
+				.attr('dy', '.35em')
+				.text(function(d) {return d.data.id;});
+
+			function pieTween(b){
+				b.innerRadius = 0;
+				var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+				return function(t){
+					return arc(i(t));
+				};
+			}
+
+			var tooltip = d3.select(this)
+				.append('div')
+				.attr('class', 'tooltip')
+				.style('background', '#eee')
+				.style('box-shadow', '0 0 5px #999999')
+				.style('color', '#333')
+				.style('display', 'none')
+				.style('font-size', tooltipFont + 'px')
+				.style('left', tooltipLeft + 'px')
+				.style('padding', tooltipPadding + 'px')
+				.style('text-align', 'center')
+				.style('top', tooltipTop + 'px')
+				.style('width', tooltipWidth + 'px')
+				.style('position', 'absolute');
+
+			tooltip.append('div')
+				.attr('class', 'label');
+
+			tooltip.append('div')
+				.attr('class', 'count');
+
+			tooltip.append('div')
+				.attr('class', 'percent');
+
+
+			path.transition()
+				.ease(d3.easeLinear)
+				.duration(1000)
+				.attrTween('d', pieTween);
+
+			texts.exit().remove();
+		})
+
+	}
+
+	chart.height = function(h) {
+		if (!arguments.length) return height;
+		height = h;
+		return chart;
+	};
+
+	chart.movement = function(m) {
+		if (!arguments.length) return movement;
+		movement = m;
+		return chart;
+	};
+
+	chart.width = function(w) {
+		if (!arguments.length) return width;
+		width = w;
+		return chart;
+	};
+
+	chart.radius = function(r) {
+		if(!arguments.length) return radius;
+		radius = r;
+		return chart;
+	};
+
+	chart.padAngle = function(a) {
+		if(!arguments.length) return padAngle;
+		padAngle = a;
+		return chart;
+	};
+
+	chart.cornerRadius = function(c){
+		if(!arguments.length) return cornerRadius;
+		cornerRadius = c;
+		return chart;
+	};
+
+	chart.tooltipWidth = function(tw){
+		if(!arguments.length) return tooltipWidth;
+		tooltipWidth = tw;
+		return chart;
+	};
+
+	chart.tooltipTop = function(top){
+		if(!arguments.length) return tooltipTop;
+		tooltipTop = top;
+		return chart;
+	};
+
+	chart.tooltipPadding = function(p){
+		if(!arguments.length) return tooltipPadding;
+		tooltipPadding = p;
+		return chart;
+	};
+
+	chart.tooltipLeft = function(l){
+		if(!arguments.length) return tooltipLeft;
+		tooltipLeft = l;
+		return chart;
+	};
+
+	chart.tooltipFont = function(f){
+		if(!arguments.length) return tooltipFont;
+		tooltipFont = f;
+		return chart;
+	};
+
+	
+
+	return chart;
+}
+
---
+++ js/main.js
@@ -0,0 +1,116 @@
+// Main.js
+var data = [];
+
+var data2 = [];
+
+$(function() {
+    // Instantiate your chart with given settings
+    var myChart = DonutChart()
+        .radius(150)
+        .height(300)
+        .width(300);
+    
+
+    // Build chart
+    var chart = d3.select('#vis')
+        .datum(data)
+        .call(myChart);
+
+    var myChart2= DonutChart()
+        .radius(150)
+        .height(300)
+        .width(300)
+        .movement(500);
+    var chart2 = d3.select('#vis2')
+        .datum(data2)
+        .call(myChart2);
+
+    var update = function(index) {
+        switch (index) {
+            case 0:
+                data = [];
+
+                data2 = [];
+                break;
+            case 1:
+                data = [{
+                    id: 'Rejected',
+                    count: 56
+                }, {
+                    id: 'Accepted',
+                    count: 44
+                }];
+
+                data2 = [{
+                    id: 'Rejected',
+                    count: 65
+                }, {
+                    id: 'Accepeted',
+                    count: 35
+                }];
+                // myChart.xAxisLabel('Men students for Department A');
+                // myChart2.xAxisLabel('Women students for Department A');
+                // var fillColor = 'red';
+                break;
+            case 2:
+                data = [{
+                    id: 'Rejected',
+                    count: 38
+                }, {
+                    id: 'Accepted',
+                    count: 62
+                }];
+
+                data2 = [{
+                    id: 'Rejected',
+                    count: 18
+                }, {
+                    id: 'Accepeted',
+                    count: 82
+                }];
+                // var fillColor = 'orange';
+                break;
+            case 3:
+                data = [{
+                    id: 'Rejected',
+                    count: 37
+                }, {
+                    id: 'Accepted',
+                    count: 63
+                }];
+
+                data2 = [{
+                    id: 'Rejected',
+                    count: 33
+                }, {
+                    id: 'Accepeted',
+                    count: 67
+                }];
+                break;
+            case 4:
+                data = [];
+
+                data2 = [];
+                break;
+            default:
+                // var fillColor = 'black';
+                break;
+        }
+        // myChart.fillColor(fillColor);
+        // myChart2.fillColor(fillColor);
+        chart.datum(data).call(myChart);
+        chart2.datum(data2).call(myChart2);
+    };
+    // Define a new scroller, and use the `.container` method to specify the desired container
+    var scroll = scroller()
+        .container(d3.select('#graphic'));
+
+    // Pass in a selection of all elements that you wish to fire a step event:
+    scroll(d3.selectAll('.step')); // each section with class `step` is a new step
+
+    // Specify the function you wish to activate when a section becomes active
+    scroll.on('active', function(index) {
+        console.log(index)
+        update(index);
+    })
+});
